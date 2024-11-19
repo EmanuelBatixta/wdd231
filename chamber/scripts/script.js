e9ebff3a4724c78bee4964d66252dcb1c21ac1a3
Emanuel Oliveira <149022040+EmanuelBatixta@users.noreply.github.com>
2024-11-19 08:12:09 -0300

    add the copywritng unicode

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -12,7 +12,7 @@ const current = document.querySelector("#currentYear")
 const modified = document.querySelector("#lastModified")
 
 var today = new Date()
-current.innerHTML =`©️${today.getFullYear()} Sanja Chamber`
+current.innerHTML =`©${today.getFullYear()} Sanja Chamber`
 modified.innerHTML=`<span id="lastModified"> ${document.lastModified}</span>`
 
 const business = document.querySelector(".business")
